26ecc9140e011e6f5e2ee0f0868ff3dec46b7f83
Joakim Nordling <joakim.nordling@ioxio.com>
2022-11-04 15:25:38 +0200

    Add descriptions to consent-configuration

--- src/consent_configuration.py
+++ src/consent_configuration.py
@@ -1,36 +1,43 @@
 from pydantic import BaseModel, Field, HttpUrl
 
+from settings import conf
+
 
 class ConsentConfiguration(BaseModel):
     class Config:
         schema_extra = {
             "title": "/.well-known/dataspace/consent-configuration.json",
             "description": "Configuration details for a consent provider on a "
-            "dataspace",
+            "dataspace. This is hosted by the consent provider on the dataspace and is "
+            f"thus available at for example `{conf.CONSENT_PROVIDER_URL}/.well-known/"
+            "dataspace/consent-configuration.json`.",
             "examples": [
                 {
-                    "issuer": "https://consent.testbed.fi",
-                    "jwks_uri": "https://consent.testbed.fi/.well-known/jwks.json",
-                    "consent_request_uri": "https://consent.testbed.fi/Consent/Request",
+                    "issuer": conf.CONSENT_PROVIDER_URL,
+                    "jwks_uri": f"{conf.CONSENT_PROVIDER_URL}/.well-known/jwks.json",
+                    "consent_request_uri": f"{conf.CONSENT_PROVIDER_URL}/Consent/"
+                    "Request",
                 }
             ],
         }
 
     issuer: HttpUrl = Field(
         ...,
-        description="",
-        examples=["https://consent.testbed.fi"],
+        description="The issuer used by the consent provider in the JWT based consent "
+        "tokens.",
+        examples=[conf.CONSENT_PROVIDER_URL],
     )
 
     jwks_uri: HttpUrl = Field(
         ...,
-        description="",
-        examples=["https://consent.testbed.fi/.well-known/jwks.json"],
+        description="The URI for the JWKs used by the consent provider when signing "
+        "tokens.",
+        examples=[f"{conf.CONSENT_PROVIDER_URL}/.well-known/jwks.json"],
     )
     consent_request_uri: HttpUrl = Field(
         ...,
-        description="",
-        examples=["https://consent.testbed.fi/Consent/Request"],
+        description="The URI at which a consent can be requested.",
+        examples=[f"{conf.CONSENT_PROVIDER_URL}/Consent/Request"],
     )
 
 
