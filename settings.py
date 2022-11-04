a3b0339a5288510215615ee3778a12a0dd141302
Joakim Nordling <joakim.nordling@ioxio.com>
2022-11-04 14:59:34 +0200

    Improve docs for dataspace-configuration docs

--- settings.py
+++ settings.py
@@ -1,12 +1,27 @@
 from pathlib import Path
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings, HttpUrl
 
 
 class Settings(BaseSettings):
     SRC_PATH: Path = Path(__file__).parent / "src"
     SCHEMAS_PATH: Path = Path(__file__).parent / "schemas"
     HTML_PATH: Path = Path(__file__).parent / "html"
+
+    DATASPACE_BASE_DOMAIN: str = "sandbox.ioxio-dataspace.com"
+    AUTHENTICATION_PROVIDER_URL: HttpUrl = "https://login.sandbox.ioxio-dataspace.com"
+    CONSENT_PROVIDER_URL: HttpUrl = "https://consent.sandbox.ioxio-dataspace.com"
+    WEBSITE_URL: HttpUrl = "https://sandbox.ioxio-dataspace.com"
+    DATASPACE_DOCS_URL: HttpUrl = "https://docs.sandbox.ioxio-dataspace.com"
+    DEFINITIONS_VIEWER_URL: HttpUrl = "https://definitions.sandbox.ioxio-dataspace.com"
+    DEVELOPER_PORTAL_URL: HttpUrl = "https://developer.sandbox.ioxio-dataspace.com"
+    PRODUCT_GATEWAY_URL: HttpUrl = "https://gateway.sandbox.ioxio-dataspace.com"
+    DEFINITIONS_GIT_URL: HttpUrl = (
+        "https://github.com/ioxio-dataspace/sandbox-definitions.git"
+    )
+    DEFINITIONS_WEB_URL: HttpUrl = (
+        "https://github.com/ioxio-dataspace/sandbox-definitions"
+    )
 
 
 conf = Settings()
--- src/dataspace_configuration.py
+++ src/dataspace_configuration.py
@@ -2,21 +2,41 @@ from typing import List
 
 from pydantic import BaseModel, Field, HttpUrl
 
+from settings import conf
+
 
 class AuthenticationProviders(BaseModel):
-    base_url: HttpUrl = Field(..., examples=["https://login.testbed.fi"])
+    base_url: HttpUrl = Field(
+        ...,
+        examples=[conf.AUTHENTICATION_PROVIDER_URL],
+        description="Base URL for the authentication provider. Appending "
+        "`/.well-known/openid-configuration` will give the full URL to the "
+        "openid-configuration.",
+    )
 
 
 class ConsentProviders(BaseModel):
-    base_url: HttpUrl = Field(..., examples=["https://consent.testbed.fi"])
+    base_url: HttpUrl = Field(
+        ...,
+        examples=[conf.CONSENT_PROVIDER_URL],
+        description="Base URL for the consent provider. Appending "
+        "`/.well-known/consent-configuration.json` will give the full URL to the "
+        "consent configuration.",
+    )
 
 
 class Definitions(BaseModel):
     git: HttpUrl = Field(
-        ..., examples=["https://github.com/Virtual-Finland/definitions.git"]
+        ...,
+        examples=[conf.DEFINITIONS_GIT_URL],
+        description="The URL to the git repository of the definitions used on the "
+        "dataspace.",
     )
     web: HttpUrl = Field(
-        ..., examples=["https://github.com/Virtual-Finland/definitions"]
+        ...,
+        examples=[conf.DEFINITIONS_WEB_URL],
+        description="The URL to a human friendly view of the definitions used on the "
+        "dataspace.",
     )
 
 
@@ -24,21 +44,24 @@ class DataspaceConfiguration(BaseModel):
     class Config:
         schema_extra = {
             "title": "/.well-known/dataspace/dataspace-configuration.json",
-            "description": "Configuration details for a dataspace and critical URLs "
-            "for it",
+            "description": "This configuration is found on the dataspace base domain, "
+            f"for example at `{conf.WEBSITE_URL}/.well-known/"
+            "dataspace/dataspace-configuration.json`. It lists all the relevant "
+            "configuration of the dataspace, such as endpoints for different dataspace "
+            "components.",
             "examples": [
                 {
-                    "dataspace_base_domain": "testbed.fi",
-                    "product_gateway_url": "https://gateway.testbed.fi",
-                    "developer_portal_url": "https://developer.testbed.fi",
-                    "docs_url": "https://docs.testbed.fi",
+                    "dataspace_base_domain": conf.DATASPACE_BASE_DOMAIN,
+                    "product_gateway_url": conf.PRODUCT_GATEWAY_URL,
+                    "developer_portal_url": conf.DEVELOPER_PORTAL_URL,
+                    "docs_url": conf.DATASPACE_DOCS_URL,
                     "authentication_providers": [
-                        {"base_url": "https://login.testbed.fi"}
+                        {"base_url": conf.AUTHENTICATION_PROVIDER_URL}
                     ],
-                    "content_providers": [{"base_url": "https://consent.testbed.fi"}],
+                    "consent_providers": [{"base_url": conf.CONSENT_PROVIDER_URL}],
                     "definitions": {
-                        "git": "https://github.com/Virtual-Finland/definitions.git",
-                        "web": "https://github.com/Virtual-Finland/definitions",
+                        "git": conf.DEFINITIONS_GIT_URL,
+                        "web": conf.DEFINITIONS_WEB_URL,
                     },
                 }
             ],
@@ -48,27 +71,27 @@ class DataspaceConfiguration(BaseModel):
         ...,
         description="The base domain for the whole dataspace, without any protocol. "
         "This is the domain at which for example the "
-        "/.well-known/dataspace-configuration.json is expected to be found.",
-        examples=["testbed.fi"],
+        "`/.well-known/dataspace/dataspace-configuration.json` is found.",
+        examples=[conf.DATASPACE_BASE_DOMAIN],
     )
 
     product_gateway_url: HttpUrl = Field(
         ...,
-        description="The URL at which the Product Gateway of the dataspace is hosted. "
-        "Must use the https:// protocol",
-        examples=["https://gateway.testbed.fi"],
+        description="The URL at which the Product Gateway of the dataspace is hosted "
+        "and on which all the data products are accessed.",
+        examples=[conf.PRODUCT_GATEWAY_URL],
     )
     developer_portal_url: HttpUrl = Field(
         ...,
-        description="The URL where the Developer Portal for the dataspace can be "
-        "found. This is where developers can register their own data sources and "
+        description="The URL where the Developer Portal for the dataspace is hosted. "
+        "This is where developers can register their own data sources and "
         "applications.",
-        examples=["https://developer.testbed.fi"],
+        examples=[conf.DEVELOPER_PORTAL_URL],
     )
     docs_url: HttpUrl = Field(
         ...,
-        description="The URL at which the dataspace API documentation can be found.",
-        examples=["https://docs.testbed.fi"],
+        description="The URL at which the API documentation is hosted.",
+        examples=[conf.DATASPACE_DOCS_URL],
     )
     authentication_providers: List[AuthenticationProviders]
     consent_providers: List[ConsentProviders]
